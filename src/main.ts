afd8cf106911d674563ad6d6c846f50c18310730
Nour Halouani <nourhalouani2@gmail.com>
2025-05-27 15:30:30 +0100

    config render

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,9 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors();
-
-//   await app.listen(3002);
-// }
-// bootstrap();
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: ['https://your-frontend-domain.com', 'https://accounts.google.com'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(3002);
 }
