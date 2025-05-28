a417e4c719e9d32535421b20336792e1e7c9ddd9
Nour Halouani <nourhalouani2@gmail.com>
2025-05-28 16:43:05 +0100

    contact us

--- prisma/migrations/20250527094941_init/migration.sql
+++ prisma/migrations/20250527094941_init/migration.sql
@@ -31,3 +31,16 @@ CREATE TABLE "FAQ" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateTable
+CREATE TABLE "ContactUs" (
+    "id" SERIAL NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "subject" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "connected" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactUs_pkey" PRIMARY KEY ("id")
+);
