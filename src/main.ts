d113e75ee1a03b52069b074e05d1ae7a247f6e49
Nour Halouani <nourhalouani2@gmail.com>
2025-05-30 15:11:17 +0100

    update user data

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // app.enableCors({});
+
+    app.enableCors({
+    origin: ['https://foxline-front.vercel.app', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(3002);
 }
